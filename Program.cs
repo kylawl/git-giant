1fd24e760673f99cddcb328dcde71878577c1a9b
Kyle Rocha <kyle@luminawesomegames.com>
2014-11-27 10:11:53 -0800

    Testing smudge and clean

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.IO;
+using System.Diagnostics;
 
 namespace GitBifrost
 {
@@ -9,15 +10,27 @@ namespace GitBifrost
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Bifrost started");
+
             if (args[0] == "activate")
             {
                 Activate();
+            }
+            else
+            {
+                foreach (string str in args)
+                {
+                    Console.WriteLine(str);
+                }
             }
         }
 
         static void Activate()
         {
             Console.WriteLine("Bifrost is now active");
+
+            Process.Start("git", "config filter.bifrost.clean 'git-bifrost clean'");
+            Process.Start("git", "config filter.bifrost.smudge 'git-bifrost smudge'");
 
             File.WriteAllText(".gitbifrost", "# Add something meaningful here");
         }
