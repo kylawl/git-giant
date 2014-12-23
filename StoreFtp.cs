0a1a212fcac3a8b015f79d4cf0b7881d66acde51
Kyle Rocha <kyle@luminawesomegames.com>
2014-12-23 00:10:14 -0800

    Fixed copyright

--- StoreFtp.cs
+++ StoreFtp.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Copyright (c) 2014 Luminawesome Games, Ltd. All Rights Reserved.
+
+using System;
 using System.Net.FtpClient;
 using System.IO;
 using System.Collections.Generic;
